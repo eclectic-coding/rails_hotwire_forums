c2bc2f750611d277806305817238cccf47e77bdc
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-09-02 07:40:33 -0400

    Create Discussion new/create actipns

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :discussions, only: [:index]
+  resources :discussions, only: [:index, :create, :new]
 
   root to: 'static#home'
 end
