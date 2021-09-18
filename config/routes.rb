ebfccd0fd34677e4b56745a89a3b6a82cfa9b703
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-09-18 18:18:00 -0400

    Add delete action to discussuions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :discussions, only: [:index, :create, :new, :edit, :update]
+  resources :discussions, only: [:index, :create, :new, :edit, :update, :destroy]
 
   root to: 'static#home'
 end
