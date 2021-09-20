5cb24be8655ad72ed182ebc5ea46d000fb58b75e
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-09-20 08:28:35 -0400

    Add show action to discussuions with turbo frame

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :discussions, only: [:index, :create, :new, :edit, :update, :destroy]
+  resources :discussions
 
   root to: 'static#home'
 end
