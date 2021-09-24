8334d9cea6751013f1c0d5af0d147bf0ff8b9c0f
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-09-24 11:05:30 -0400

    Close discussions in real time

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -34,7 +34,7 @@ class DiscussionsController < ApplicationController
   def update
     respond_to do |format|
       if @discussion.update(discussion_params)
-        @discussion.broadcast_replace(partial: "discussions/header", locals: { discussion: @discussion })
+        @discussion.broadcast_replace(partial: "discussions/header", locals: {discussion: @discussion})
 
         if @discussion.saved_change_to_category_id?
           old_category_id, new_category_id = @discussion.saved_change_to_category_id
@@ -50,6 +50,17 @@ class DiscussionsController < ApplicationController
           old_category.reload.broadcast_replace_to("categories")
           new_category.reload.broadcast_replace_to("categories")
         end
+
+        if @discussion.saved_change_to_closed?
+          @discussion.broadcast_action_to(
+            @discussion,
+            action: :replace,
+            target: "new_post_form",
+            partial: "discussions/posts/form",
+            locals: {post: @discussion.posts.new}
+          )
+        end
+
         format.html { redirect_to @discussion, notice: "Discussion updated" }
       else
         format.html { render :edit, status: :unprocessable_entity }
